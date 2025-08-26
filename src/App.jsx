cb9b374084e17d7e968a98385957c9aaaf4503be
mohdfaiz2008 <mohdfaiz.faiz2008@gmail.com>
2025-08-26 23:16:32 +0530

    react router completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Login/Login";
-// import SignUp from "./Signup/Singup";
+import SignUp from "./Signup/Singup";
 
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center app">
-      <Login/>
-      {/* <SignUp/> */}
+      <Routes >
+        <Route path={"/"} element={<Login/>}/>
+        <Route path={"/signup"} element={<SignUp/>}/>
+      </Routes>
     </div>
   );
 }
